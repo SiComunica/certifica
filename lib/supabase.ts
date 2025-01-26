78eec411bd1198e6cdc62700f718a8bc9b8f41a1
SiComunica <francescocro76@gmail.com>
2025-01-26 20:42:08 +0100

    Fix: aggiornate credenziali Supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://tviiynqkosrflhffouwj.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2aWl5bnFrb3NyZmxoZmZvdXdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5MTM5NTIsImV4cCI6MjA1MzQ4OTk1Mn0.EFt0AP3TV2wxpe0yBEL2SyvrODsNa48YiCF4gkqeyx8'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
